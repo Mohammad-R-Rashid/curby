581111c0042e143819883c13e6bc5e3be82298d4
Mohammad Rashid <mohammad.rashid7337@gmail.com>
2026-04-18 12:43:04 -0500

    Initial Commit

---
+++ curby/curbyApp.swift
@@ -0,0 +1,17 @@
+//
+//  curbyApp.swift
+//  curby
+//
+//  Created by Mohammad Rashid on 11/1/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct curbyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
